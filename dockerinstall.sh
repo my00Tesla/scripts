45654b5dde2ea54ec7543cb8d6c57f744ee70c11
my00Tesla <42440404+my00Tesla@users.noreply.github.com>
2018-11-24 14:01:41 -0500

    Create dockerinstall.sh

---
+++ dockerinstall.sh
@@ -0,0 +1,8 @@
+# Install Docker-CE onto Ubuntu
+#
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
